9292239edb903800de3ffe55f0ff5b6a1fd7cac7
IagoMC <113433698+IagoMC@users.noreply.github.com>
2023-03-17 13:46:16 +0100

    Add files via upload

---
+++ Cliente/src/URL_SitioWeb.js
@@ -0,0 +1,5 @@
+const BASE_REAL_URL = "http://localhost:8000"
+const BASE_MOCK_URL = "https://78872ca0-ef3a-4149-ab8e-f047306e205b.mock.pstmn.io"
+
+const BASE_URL =  BASE_MOCK_URL;
+export default BASE_URL;
