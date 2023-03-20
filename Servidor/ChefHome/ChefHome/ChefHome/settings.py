26fb99f33644ce9e0a7c31e19bb2c108635a0e91
IagoMC <113433698+IagoMC@users.noreply.github.com>
2023-03-20 12:37:33 +0100

    Update settings.py

--- Servidor/ChefHome/ChefHome/ChefHome/settings.py
+++ Servidor/ChefHome/ChefHome/ChefHome/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',	
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'ChefHome.urls'
 
